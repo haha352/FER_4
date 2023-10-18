494acbfca8658f1a15e6011c991dbac9e4510ccc
haha352 <phamtanty789@gmail.com>
2023-10-18 10:50:55 +0700

    Sườn

---
+++ src/components/Navigation.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useContext } from 'react'
+import { ThemeContext } from './ThemeContext'
+export default function Navigation() {
+  const { theme, toggle, dark } = useContext(ThemeContext)
+  return (
+    <div className='body-navi'>
+      <nav style={{ backgroundColor: theme.backgroundColor, color: theme.color }}
+        className="navbar navbar-expand-sm fixed-top">
+      
+      <button className="navbar-toggler" type="button" data-toggle="collapse"
+        data-target="#Navbar">
+        <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="Navbar">
+        <ul className="navbar-nav mr-auto">
+                <li className="nav-item active"><a className="nav-link" href="/">
+                <span className="fa fa-home fa-lg"></span>Home</a></li>
+                <li className="nav-item"><a className="nav-link"  href="new">
+                <span className="fa fa-list fa-lg"></span>Series</a></li>
+                <li className="nav-item"><a className="nav-link"  href="about">
+                <span className="fa fa-info fa-lg"></span>About</a></li>
+                <li className="nav-item"><a className="nav-link"  href="contact">
+                <span className="fa fa-address-card fa-lg"></span>Contact</a></li>
+            </ul>
+          <div style={{position: 'relative'}}>
+          <a className='switch-mode' href='#' onClick={toggle}
+          style={{
+            backgroundColor: theme.backgroundColor,
+            color: theme.color,
+            outline: 'none'
+          }} data-testid="toggle-theme-btn"
+        >
+          Switch Nav to {!dark ? 'Dark' : 'Light'} mode
+         </a>
+         </div>
+      
+      </div>
+      </nav>
+      
+      </div>
+      
+  )
+}
